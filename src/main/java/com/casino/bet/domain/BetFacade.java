ecddd2690633c792d8632123974e19e9222334f9
Maksymilian Ulanecki <maks.u@vp.pl>
2024-03-17 22:18:10 +0100

    created bet controller

--- src/main/java/com/casino/bet/domain/BetFacade.java
+++ src/main/java/com/casino/bet/domain/BetFacade.java
@@ -9,10 +9,12 @@ import com.casino.bet.exception.BetNotFound;
 import com.casino.bet.exception.InvalidBetCombination;
 import com.casino.game.domain.GameFacade;
 import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Component
 @AllArgsConstructor
 public class BetFacade {
 
@@ -39,38 +41,38 @@ public class BetFacade {
                 .id(betDto.id())
                 .placed(true)
                 .placedAmount(betDto.amount())
-                .timeOfPlacement(betDto.timeOfPlacement())
+                .timeOfPlacement(timeOfPlacement)
                 .betType(betDto.betType())
                 .build();
     }
 
-    List<BetDto> findAllBets() {
+    public List<BetDto> findAllBets() {
         return repository.findAll()
                 .stream()
                 .map(Bet::toDto)
                 .toList();
     }
 
-    List<BetDto> findAllBetsByUserId(Long userId) {
+    public List<BetDto> findAllBetsByUserId(Long userId) {
         return repository.findAllByUserId(userId)
                 .stream()
                 .map(Bet::toDto)
                 .toList();
     }
 
-    List<BetDto> findAllBetsByGameId(Long gameId) {
+    public List<BetDto> findAllBetsByGameId(Long gameId) {
         return repository.findAllByGameId(gameId)
                 .stream()
                 .map(Bet::toDto)
                 .toList();
     }
 
-    BetDto findBetById(Long id) {
+    public BetDto findBetById(Long id) {
         return repository.findById(id)
                 .orElseThrow(BetNotFound::new).toDto();
     }
 
-    List<BetDto> findAllBetsByGameIdAndUserId(Long gameId, Long userId) {
+    public List<BetDto> findAllBetsByGameIdAndUserId(Long gameId, Long userId) {
         return repository.findAllByGameIdAndUserId(gameId, userId)
                 .stream()
                 .map(Bet::toDto)
--- src/main/java/com/casino/bet/dto/PlaceBet.java
+++ src/main/java/com/casino/bet/dto/PlaceBet.java
@@ -1,6 +1,7 @@
 package com.casino.bet.dto;
 
 import com.casino.bet.domain.BetType;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 
 import java.math.BigDecimal;
@@ -9,6 +10,7 @@ import java.math.BigDecimal;
 public record PlaceBet(
         Long userId,
         BigDecimal amount,
+        @JsonFormat(shape = JsonFormat.Shape.STRING)
         BetType betType
 ) {
 }
