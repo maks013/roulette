c883cda1986eda43f56d9d333fd4afb56fe36da8
Maksymilian Ulanecki <maks.u@vp.pl>
2024-04-20 23:51:59 +0200

    added draw results calculation to scheduler

--- src/main/java/com/casino/infrastructure/game/scheduler/GameScheduler.java
+++ src/main/java/com/casino/infrastructure/game/scheduler/GameScheduler.java
@@ -1,5 +1,6 @@
 package com.casino.infrastructure.game.scheduler;
 
+import com.casino.bet.domain.BetFacade;
 import com.casino.game.domain.GameFacade;
 import com.casino.game.dto.GameDto;
 import lombok.AllArgsConstructor;
@@ -7,16 +8,20 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 @Component
 @Log4j2
 @AllArgsConstructor
 public class GameScheduler {
 
     private final GameFacade gameFacade;
+    private final BetFacade betFacade;
 
     @Scheduled(fixedRate = 60000)
     public void rollNewGameEveryMinute(){
         GameDto gameDto = gameFacade.roll();
+        betFacade.calculateDrawResults(LocalDateTime.now());
         log.info("New game rolled " + gameDto);
     }
 }
